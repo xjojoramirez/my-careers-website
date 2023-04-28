74b02009aa2d1f6d9cd07f1d8abd8e2a1dabc243
xjojoramirez <31147978+xjojoramirez@users.noreply.github.com>
2023-04-28 15:14:11 +0000

    Add sample data

--- app.py
+++ app.py
@@ -2,11 +2,42 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Benglaru, India',
+  'salary': 'Rs. 1,00,000'
+}, {
+  'id': 2,
+  'title': 'Software Engineer',
+  'location': 'Delhi, India',
+  'salary': 'Rs. 1,50,000'
+}, {
+  'id': 3,
+  'title': 'Frontend Developer',
+  'location': 'Remote',
+  'salary': 'Rs. 1,60,000'
+}, {
+  'id': 3,
+  'title': 'Backend Developer',
+  'location': 'San Francisco, USA',
+  'salary': '$120,000'
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS)
+
+
+@app.route("/jobs")
+def list_jobs():
+  return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
+
+# {% for job in jobs %}
+#       {% include 'blogitem.html' %}
+#     {% endfor %}
